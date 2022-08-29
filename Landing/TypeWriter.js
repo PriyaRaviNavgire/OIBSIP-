8f47aa24ac6ac3625132b2905567e3431fa275f9
Priya Ravi Navgire <109460668+PriyaRaviNavgire@users.noreply.github.com>
2022-08-29 10:52:18 +0530

    Update TypeWriter.js

--- Landing/TypeWriter.js
+++ Landing/TypeWriter.js
@@ -1,1 +1,28 @@
+var arr = ['Open Source', 'Secure', 'Free', 'Fast', '', 'Love'];
+var b = "";
+var c = 0;
+var tim;
+var bli = 0;
+function f() {
+    tim = setInterval(myFun, 100);
+}
+f();
+var element = document.getElementById("linuxSpan");
+var cur = 0;
+function myFun() {
+    b = b + arr[cur][c]
+    document.getElementById("linuxSpan").innerHTML = b;
+    c = c + 1;
+    if (c == arr[cur].length) {
+        c = 0;
+        b = "";
+        pause();
+        cur = cur + 1;
+    };
+    if (cur == 4) { cur = 0 }
+}
 
+function pause() {
+    clearInterval(tim);
+    setTimeout(f, 2500);
+}
